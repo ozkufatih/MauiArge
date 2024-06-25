764a193244e5afe46eba9d85c77f0dc2cc2f1ad1
Fatih Özkurt <ozkufatih@gmail.com>
2024-06-25 18:24:55 +0300

    Bug fix.

--- MauiApp1/Controls/CategoryAssetView.xaml.cs
+++ MauiApp1/Controls/CategoryAssetView.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui.Views;
+
 namespace MauiApp1.Controls;
 
 public partial class CategoryAssetView : ContentView
@@ -5,5 +7,5 @@ public partial class CategoryAssetView : ContentView
 	public CategoryAssetView()
 	{
 		InitializeComponent();
-	}
+    }
 }
